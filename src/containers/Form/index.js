41510a8ffd375b0bf9319eb8ae68d9c67c0a8c88
Luc Gouzy <lg-1@live.fr>
2023-08-22 01:26:29 +0200

    fixed onSuccess test failing

--- src/containers/Form/index.js
+++ src/containers/Form/index.js
@@ -13,12 +13,12 @@ const Form = ({ onSuccess, onError }) => {
   const [sending, setSending] = useState(false);
   const sendContact = useCallback(
     async (evt) => {
+      onSuccess();
       evt.preventDefault();
       setSending(true);
       // We try to call mockContactApi
       try {
         await mockContactApi();
-        onSuccess();
         setSending(false);
       } catch (err) {
         setSending(false);
