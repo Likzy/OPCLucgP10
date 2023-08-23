a65e9154cb56d2086d2fa9b7e2dbcad12d5f6ff3
Luc Gouzy <lg-1@live.fr>
2023-08-23 16:04:40 +0200

    fixed console error

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -9,7 +9,7 @@ const Slider = () => {
   const [index, setIndex] = useState(0);
 
   const byDateDesc = data?.focus.sort((evtA, evtB) =>
-    new Date(evtA.date) > new Date(evtB.date) ? -1 : 1
+    new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
   );
   const nextCard = () => {
     setTimeout(
@@ -52,6 +52,7 @@ const Slider = () => {
                       type="radio"
                       name="radio-button"
                       checked={index === radioIdx}
+                      readOnly
                     />
                   );
                 })}
